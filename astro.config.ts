cef90c57320ae03da75e7eb3a05c3b8f3f4954eb
Spenser Wellen <spenserwellen@gmail.com>
2024-10-29 19:01:07 -0700

    completed hero section

--- astro.config.ts
+++ astro.config.ts
@@ -1,90 +1,104 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
-import icon from 'astro-icon';
-import compress from 'astro-compress';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import type { AstroIntegration } from 'astro';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
 
 import astrowind from './vendor/integration';
 
-import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
+import {
+	lazyImagesRehypePlugin,
+	readingTimeRemarkPlugin,
+	responsiveTablesRehypePlugin,
+} from './src/utils/frontmatter';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const hasExternalScripts = false;
-const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroIntegration)[] = []) =>
-  hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
+const whenExternalScripts = (
+	items: (() => AstroIntegration) | (() => AstroIntegration)[] = []
+) =>
+	hasExternalScripts
+		? Array.isArray(items)
+			? items.map((item) => item())
+			: [items()]
+		: [];
 
 export default defineConfig({
-  output: 'static',
+	output: 'static',
 
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    mdx(),
-    icon({
-      include: {
-        tabler: ['*'],
-        'flat-color-icons': [
-          'template',
-          'gallery',
-          'approval',
-          'document',
-          'advertising',
-          'currency-exchange',
-          'voice-presentation',
-          'business-contact',
-          'database',
-        ],
-      },
-    }),
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		mdx(),
+		icon({
+			include: {
+				tabler: ['*'],
+				'flat-color-icons': [
+					'template',
+					'gallery',
+					'approval',
+					'document',
+					'advertising',
+					'currency-exchange',
+					'voice-presentation',
+					'business-contact',
+					'database',
+				],
+			},
+		}),
 
-    ...whenExternalScripts(() =>
-      partytown({
-        config: { forward: ['dataLayer.push'] },
-      })
-    ),
+		...whenExternalScripts(() =>
+			partytown({
+				config: { forward: ['dataLayer.push'] },
+			})
+		),
 
-    compress({
-      CSS: true,
-      HTML: {
-        'html-minifier-terser': {
-          removeAttributeQuotes: false,
-        },
-      },
-      Image: false,
-      JavaScript: true,
-      SVG: false,
-      Logger: 1,
-    }),
+		compress({
+			CSS: true,
+			HTML: {
+				'html-minifier-terser': {
+					removeAttributeQuotes: false,
+				},
+			},
+			Image: false,
+			JavaScript: true,
+			SVG: false,
+			Logger: 1,
+		}),
 
-    astrowind({
-      config: './src/config.yaml',
-    }),
-  ],
+		astrowind({
+			config: './src/config.yaml',
+		}),
+	],
 
-  image: {
-    domains: ['cdn.pixabay.com'],
-  },
+	image: {
+		domains: ['cdn.pixabay.com'],
+		service: passthroughImageService(),
+	},
 
-  markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin],
-    rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
-  },
+	markdown: {
+		remarkPlugins: [readingTimeRemarkPlugin],
+		rehypePlugins: [
+			responsiveTablesRehypePlugin,
+			lazyImagesRehypePlugin,
+		],
+	},
 
-  vite: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
-    },
-  },
+	vite: {
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	},
 });
